786dfe1f872787d8a4f92865ab42990237862327
Eatcoding <trivialcoding6@gmail.com>
2025-04-02 14:49:50 +0900

    pr 템플릿 테스트

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.router import api_router
 
-app = FastAPI(title="배포 연습용 API")
+app = FastAPI(title="배포 연습용 API test")
 
 # API 라우터 등록
 app.include_router(api_router)
